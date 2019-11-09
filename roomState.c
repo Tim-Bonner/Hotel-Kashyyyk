4210bffde1b3a739fba3a2dd29718b9d22d8b801
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-11-09 09:08:45 +0000

    Update roomState.c

--- roomState.c
+++ roomState.c
@@ -11,7 +11,13 @@ int roomState(array){
 int main() {
     //                null   room1   room2   room3 room4
     int room[5][100]={{0,0}, {0,100},{0,85},{0,75},{0,50} } ;
+    char* user[3][2]= {{"Barney", "Morris", "Bmorris3281"}};
+    int userInfo[3][6]= {{4,2,2,3,0,3}};
 
 
-    printf("Room 1 is %s and costs %d", roomState(room[1][0]), room[1][1]);
+    printf("%s wants to be in room%d\n", user[0][0], userInfo[0][5]);
+
+    printf("Room 3 is %s and costs %d", roomState(room[userInfo[0][5]][0]), room[userInfo[0][5]][1]);
+    
+    
 }
