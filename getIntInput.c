b409bb02d27d12c1ecdb7c28f22a784ffd67ee32
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-11-09 08:35:29 +0000

    Update getIntInput.c

--- getIntInput.c
+++ getIntInput.c
@@ -6,14 +6,20 @@ int getInput(print){
         printf("%s", print);
         fflush(stdin);
         error = scanf("%d", &input);
+        if(error!=1){
+            printf("Input is not an integer! please try again.\n");
+        }
+        else if(input<0){
+            printf("Number is negative! Please try again.\n");
+        }
     }
     return input;
 }
 int main() {
 
-    //Example asks how many days 
+    //Example asks how many days
     int days=0;
     days = getInput("How many days");
     printf("%d",days);
-return 0;
+    return 0;
 }
