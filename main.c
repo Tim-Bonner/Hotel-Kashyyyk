e936a1abae49ef90ca3b51c424e7c8498782cfef
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-11-09 13:05:52 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,111 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+//**** GLOBAL VARIABLE DECLERATION ****
+int roomInt[5][8]={ {0,0,0,0,0,0,0,0} , {0,100,0,0,0,0,0,0}, {1,85,0,0,0,0,0,0}, {0,75,0,0,0,0,0,0},{0,50,4,3,1,3,1,2}}; 
+char* roomStr[5][3];
+int loop =1, arrayCollums=0; 
+int input=0;
+
+//*************************************
+int roomState(array){
+    if(array==0){
+        return "vacant";
+    }
+    else{
+        return "taken";
+    }
+}
+
+int getIntInput(print){
+    int error=0,input=1;
+    while (error != 1 || input<0) {
+        printf("%s", print);
+        fflush(stdin);
+        error = scanf("%d", &input);
+        if(error!=1){
+            printf("Input is not an integer! please try again.\n");
+        }
+        else if(input<0){
+            printf("Number is negative! Please try again.\n");
+        }
+    }
+    return input;
+}
+
+int getStrInput(print){
+    int error=0;
+    char* input;
+    while (error != 1) {
+        printf("%s", print);
+        fflush(stdin);
+        error = gets("%s", &input);
+        if(error!=1){
+            printf("Input is not a string! please try again.\n");
+        }
+        else if(input<0){
+            printf("Number is negative! Please try again.\n");
+        }
+    }
+    return input;
+}
+    
+int roomSelect(){
+    int error=0;
+    input=0;
+    while (error != 1 || input<0 || input>4 || roomInt[input][0]==1) { //forces user to choose a real vacant room
+        for(int i =1; i<5; i++){
+            printf("\n%d - Room%d costs £%d and is %s\n", i,i, roomInt[i][1], roomState(roomInt[i][0]));
+        }
+        printf("Please select room number: ");
+        fflush(stdin);
+        error = scanf("%d", &input);
+        //printf("%d", error);
+        if(error!=1){
+            printf("\nInput must be a room number. Please try again.\n");
+        }
+        else if(input<0 || input>4){
+            printf("\nRoom number must be between 1 and 4. Please try again.\n");
+        }
+        else if(roomInt[input][0]==1){
+            printf("Room is not vacant. Please select a vacant room.");
+        }
+    }
+}
+
+int detailFill(){
+    roomStr[input][0]=getStrInput("What is your firstname: ");
+    printf("%s", roomStr[1][0]);
+}
+
+
+int main()
+{
+    arrayCollums = sizeof(roomInt)/sizeof(roomInt[0]);
+ 
+    while(loop==1){
+        switch(getIntInput("1 - Check In\n2 - Book a Dinner Table\n3 - Check Out\n4 - Quit\nChoose an option:")){
+            
+            case 1:
+                roomSelect();
+                detailFill();
+        }
+    }   
+    
+    
+} 
+    
+        
+        
+    
+        
+    //printf("%d", roomInt[4][7]);
+    
+
