6d484cb9d2cbd3630ce5fd36f06de946bd467781
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-11-10 09:20:15 +0000

    Create arrays.c
    An example of how to deal with strings

---
+++ arrays.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+
+
+int main()
+{
+
+    
+    #include <stdio.h>
+
+    char word[2][1][20];
+    
+    printf("Enter data : ");
+    scanf("%s", word[1][1]);    // Read and store
+    printf("%s\n", word[1][1]);    // Print
+    
+    printf("Enter data : ");
+    scanf("%s", word[0][1]);    // Read and store
+    printf("%s\n", word[0][1]);    // Print
+    return 0;
+}
